6c62ad97a5b5654d6983a0733c7d341a67d64d94
Bart Thate <operbot100@gmail.com>
2023-02-05 18:54:18 +0100

    update opq.utl

--- opq/utl.py
+++ opq/utl.py
@@ -20,9 +20,7 @@ def __dir__():
             'fntime',
             'locked',
             'name',
-            'privileges',
             'spl',
-            'wait'
            )
 
 
@@ -141,29 +139,9 @@ def name(obj):
     return None
 
 
-def privileges(username):
-    if os.getuid() != 0:
-        return
-    try:
-        pwnam = pwd.getpwnam(username)
-    except KeyError:
-        username = getpass.getuser()
-        pwnam = pwd.getpwnam(username)
-    os.setgroups([])
-    os.setgid(pwnam.pw_gid)
-    os.setuid(pwnam.pw_uid)
-
-
 def spl(txt):
     try:
         res = txt.split(",")
     except (TypeError, ValueError):
         res = txt
     return [x for x in res if x]
-
-
-def wait(func=None):
-    while 1:
-        time.sleep(1.0)
-        if func:
-            func()
